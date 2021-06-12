3292f2f82cd492f0e6ce12e4daf7cf3922215e4d
Anne Do <22310076+annedo@users.noreply.github.com>
2021-06-11 22:40:49 -0400

    Updates

---
+++ precache-manifest.6ab3ebfca0258515ed09317a8133f4fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16cdfc3ad70058bb1602",
+    "url": "/react_tutorial/static/css/main.e3f47334.chunk.css"
+  },
+  {
+    "revision": "16cdfc3ad70058bb1602",
+    "url": "/react_tutorial/static/js/main.16cdfc3a.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/react_tutorial/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "997f229cf593d61522b4",
+    "url": "/react_tutorial/static/js/runtime~main.997f229c.js"
+  },
+  {
+    "revision": "099f4c7d88b3f8674220f06a7a132d7a",
+    "url": "/react_tutorial/index.html"
+  }
+];
